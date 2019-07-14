8dd528926427b134e1b663ffb670b5df8b195b67
fumi35g <bm3291982@gmail.com>
2019-07-14 19:24:46 +0900

    chap3 p115まで

---
+++ product/mst_product.sql
@@ -0,0 +1,6 @@
+create table shop.mst_product (
+	code int not null primary key auto_increment,
+	name varchar(30),
+	price int,
+	gazou varchar(30)
+)
---
+++ staff/staff_branch.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ろくまる農園</title>
+</head>
+<body>
+<?php
+
+if(isset($_POST['disp'])==true){
+    if(isset($_POST['staffcode']) == false){
+        header('Location:staff_ng.php');
+        exit();
+    }
+    $staff_code=$_POST['staffcode'];
+    header('Location:staff_disp.php?staffcode='.$staff_code);
+        exit();
+}
+
+if(isset($_POST['add'])==true){
+    header('Location:staff_add.php');
+    exit();
+}
+
+if(isset($_POST['edit'])==true){
+    if(isset($_POST['staffcode']) == false){//追加
+        header('Location:staff_ng.php');    //追加
+        exit();                             //追加
+    }
+    $staff_code=$_POST['staffcode'];
+    header('Location:staff_edit.php?staffcode='.$staff_code);
+    exit();
+}
+
+if(isset($_POST['delete'])==true){
+    $staff_code=$POST['staffcode'];
+    header('Location:staff_delete.php?staffcode='.$staff_code);
+    exit();
+}
+
+?>
+</body>
+
+</html>
